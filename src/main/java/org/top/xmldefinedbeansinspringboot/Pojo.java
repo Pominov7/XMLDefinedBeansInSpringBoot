e46cb1c9633551efc7a40fe6d8d2f2ba966baa56
Pominov7 <Pominov7@mail.ru>
2022-11-06 18:13:35 +0700

    Initial commit

---
+++ src/main/java/org/top/xmldefinedbeansinspringboot/Pojo.java
@@ -0,0 +1,17 @@
+package org.top.xmldefinedbeansinspringboot;
+
+public class Pojo {
+
+    private String field;
+
+    public Pojo() {
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
---
+++ src/main/java/org/top/xmldefinedbeansinspringboot/SpringBootXmlApplication.java
@@ -0,0 +1,28 @@
+package org.top.xmldefinedbeansinspringboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.boot.CommandLineRunner;
+
+@Configuration
+@EnableAutoConfiguration
+@ImportResource("classpath:beans.xml")
+public class SpringBootXmlApplication implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootXmlApplication.class);
+
+    @Autowired
+    private Pojo pojo;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootXmlApplication.class, args);
+    }
+
+    public void run(String... args) {
+        logger.info(pojo.getField());
+    }
+}
